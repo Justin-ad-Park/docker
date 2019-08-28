4ece5b4006d0a0005c1f347f67af9bcb0d69a7e1
Justin-ad-Park <justin.ad.park@gmail.com>
2019-08-28 20:32:14 +0900

    Add mysql-data and shell

---
+++ mysql-data/02.run.sh
@@ -0,0 +1,2 @@
+echo docker container run -d --name mysql-data justinpark/mysql-data:latest
+docker container run -d --name mysql-data justinpark/mysql-data:latest
---
+++ mysql-data/06.mount_n_backup.sh
@@ -0,0 +1,1 @@
+docker container run -v ${PWD}/tmp:/tmp --volumes-from mysql-data busybox tar cvzf /tmp/mysql-backup.tar.gz /var/lib/mysql
