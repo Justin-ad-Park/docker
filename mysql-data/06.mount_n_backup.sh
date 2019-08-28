04ecb2edc1c29a419270c02ed3d87828a2a49dd6
Justin-ad-Park <justin.ad.park@gmail.com>
2019-08-28 20:35:36 +0900

    Modify compress and mount shell

--- mysql-data/06.mount_n_backup.sh
+++ mysql-data/06.mount_n_backup.sh
@@ -1,1 +1,4 @@
-docker container run -v ${PWD}/tmp:/tmp --volumes-from mysql-data busybox tar cvzf /tmp/mysql-backup.tar.gz /var/lib/mysql
+docker container run -v ${PWD}/tmp:/tmp \
+  --volumes-from mysql-data \
+  busybox \
+  tar cvzf /tmp/mysql-backup.tar.gz /var/lib/mysql
